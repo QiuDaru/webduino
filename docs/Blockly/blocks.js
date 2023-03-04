027108464fed24a539f19bb32ccc0d11519fbb1c
邱達儒 <1234567si2k@gmail.com>
2023-03-04 13:24:06 +0800

    BLOCKLY

---
+++ docs/Blockly/blocks.js
@@ -0,0 +1,9 @@
+Blockly.Blocks['blocktype'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("ZXCVBNM");
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ docs/Blockly/javascript.js
@@ -0,0 +1,5 @@
+Blockly.Arduino['blocktype'] = function(block) {
+  // TODO: Assemble Arduino into code variable.
+  var code = '...;\n';
+  return code;
+};
